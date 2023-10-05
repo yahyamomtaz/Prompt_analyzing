a632a495ae339088ff92ce953812f0fcfe7275c1
yahyamomtaz <yahyamomtaz@gmail.com>
2023-10-05 22:46:00 +0200

    sidebar added

--- main.py
+++ main.py
@@ -10,12 +10,23 @@ matplotlib.use('TkAgg')
 
 load_dotenv()
 
-API_KEY = os.environ['OPENAI_API_KEY']
+#API_KEY = os.environ['OPENAI_API_KEY']
+st.set_page_config(page_title="Analyzer")
+st.title('PandasAI Prompt Analyzer')
+
+with st.sidebar:
+    API_KEY = st.text_input("Enter your API KEY here:")
+    
+    st.markdown('''
+    Made by **Yahya Momtaz**
+    - [GitHub](https://github.com/yahyamomtaz)
+    - [Kaggle](https://www.kaggle.com/yahyamomtaz)
+    - [Linkedin](https://www.linkedin.com/in/yahya-momtaz-601b34108)
+    ''')
+
 llm = OpenAI(api_token=API_KEY)
 padndasai = PandasAI(llm)
 
-
-st.title('PandasAI Prompt Analyzer')
 file = st.file_uploader("Upload your CSV file", type=['csv'])
 
 if file is not None:
