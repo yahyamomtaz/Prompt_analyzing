6a239e362d350856a4332f7c50b8314150cc8230
yahyamomtaz <yahyamomtaz@gmail.com>
2023-10-05 22:37:45 +0200

    matplotlib and generating added

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+from pandasai import PandasAI
+from pandasai.llm.openai import OpenAI
+from dotenv import load_dotenv
+import os
+import streamlit as st
 import pandas as pd
-from pandasai import SmartDatalake
-from pandasai.llm import OpenAI
+import matplotlib
+
+matplotlib.use('TkAgg')
+
+load_dotenv()
+
+API_KEY = os.environ['OPENAI_API_KEY']
+llm = OpenAI(api_token=API_KEY)
+padndasai = PandasAI(llm)
+
+
+st.title('PandasAI Prompt Analyzer')
+file = st.file_uploader("Upload your CSV file", type=['csv'])
+
+if file is not None:
+    df = pd.read_csv(file)
+    st.write(df.head())
+    
+    prompt = st.text_area("Enter your prompt here:")
+    
+    if st.button("Generate"):
+        if prompt:
+            with st.spinner("Generating, Please wait..."):
+                st.write(padndasai.run(df,prompt=prompt))
+        else:
+            st.write("Please Enter a Prompt")
